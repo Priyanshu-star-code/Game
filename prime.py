d590584674ecd380f254d7cf520b0b7aa81bc5a0
pro-player <vpriyanshu708@gmail.com>
2023-11-08 16:48:29 +0530

    adding prime.py file

---
+++ prime.py
@@ -0,0 +1,11 @@
+#   Write a program to display all prime numbers within a range.
+num=int(input("Enter your range number\n "))
+num1=int(input("Enter your range number1 \n "))
+print ("The Prime Numbers in the range are: ")  
+for number in range (num, num1+ 1):  
+    if number > 1:  
+        for i in range (2, number):  
+            if (number % i) == 0:  
+                break  
+        else:  
+            print (number)  
