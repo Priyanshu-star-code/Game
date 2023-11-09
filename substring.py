5a08e71ac5f9d10222a2349cbb1b182cbd0b46b6
pro-player <vpriyanshu708@gmail.com>
2023-11-09 15:53:35 +0530

    update

---
+++ substring.py
@@ -0,0 +1,9 @@
+#  Check if a substring is present in a given string.
+main_string="Hello, How are you "
+#search word 
+sub_string="how"
+# check condition 
+if sub_string in main_string:
+    print("This is a substring found" )
+else:
+    print(" this is not sunstring ")
