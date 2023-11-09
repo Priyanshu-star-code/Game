3b57b82b506a98a01a36ec355c030a4d9d930d5f
pro-player <vpriyanshu708@gmail.com>
2023-11-09 15:28:34 +0530

    Update file

--- prime.py
+++ prime.py
@@ -3,9 +3,9 @@ num=int(input("Enter your range number\n "))
 num1=int(input("Enter your range number1 \n "))
 print ("The Prime Numbers in the range are: ")  
 for number in range (num, num1+ 1):  
-    if number > 1:  
+    if number > 1:    # check greater than 1
         for i in range (2, number):  
-            if (number % i) == 0:  
+            if (number % i) == 0:  #checks if the current number is divisible by i
                 break  
         else:  
-            print (number)  
+            print (number)
---
+++ unique_element.py
@@ -0,0 +1,4 @@
+# write a program to print unique element from the list
+list_ori=["hello",5,'Hii',90,7.6,5 ,5]
+unique_element=list(set(list_ori))
+print(unique_element)
