fc6a2ad58966b1348b8baa5167d23aaa0a1438a2
pro-player <vpriyanshu708@gmail.com>
2023-11-16 23:34:59 +0530

    print  file

---
+++ print_key.py
@@ -0,0 +1,11 @@
+# Given dictionary
+my_dict = {
+    'name': 'Priyanshu ',
+    'age': 21,
+    'city': 'Uttar Pradesh',
+    'occupation': 'Engineer'
+}
+
+# Printing all keys from the dictionary
+for key in my_dict.keys():  #  This program uses the keys() method of the dictionary
+    print(key)
