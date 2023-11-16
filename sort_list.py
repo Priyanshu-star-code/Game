a872fcb850ff42b42c3c314f8100ba9049255203
pro-player <vpriyanshu708@gmail.com>
2023-11-16 23:13:55 +0530

    New file

---
+++ sort_list.py
@@ -0,0 +1,9 @@
+# sort a list of integers in descending order?
+integer_list = [5, 2, 8, 1, 9, 3 , 3 ]
+
+# Sort the list in decreasing order
+# reverse=True parameter is used to sort the list in descending order.
+sorted_list = sorted(integer_list, reverse=True)
+
+# Print the sorted list
+print("Sorted list in decreasing order:", sorted_list)
